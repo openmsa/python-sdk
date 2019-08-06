7ceec8633bffcdceac7c3f49e5a22423e993e7df
Eduardo Ferreira <efe@ubiqube.com>
2019-08-06 13:57:07 +0100

    MSA-5177 Implement functions

---
+++ tests/test_repository.py
@@ -0,0 +1,26 @@
+"""
+Test Repository
+"""
+from urllib.parse import urlencode
+from unittest.mock import patch
+from util import repository_fixture  # pylint: disable=unused-import
+
+# pylint: disable=redefined-outer-name
+
+
+def test_file_update_comment(repository_fixture):
+    """
+    Test file update comment
+    """
+
+    with patch('msa_sdk.msa_api.MSA_API.call_post') as mock_call_post:
+        repository = repository_fixture
+        repository.file_update_comment(
+            'Configuration/ABR/ABRA1570/FORTINET/timezone',
+            'Comment value')
+
+        data = urlencode({'uri': 'Configuration/ABR/ABRA1570/FORTINET/timezone',
+                          'comment': 'Comment value'})
+
+        assert repository.path == '/repository/comment?{}'.format(data)
+        mock_call_post.assert_called_once_with()
