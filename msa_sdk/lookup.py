6e82278c63c8049c9d03b386d185ad8c79f8dba2
Eduardo Ferreira <efe@ubiqube.com>
2019-09-02 14:34:01 +0100

    MSA-5839 - Implement lookup functions
    Functions implemented:
    
    - lookup_list_devices_by_customer_reference
    - lookup_list_device_ids
    - lookup_list_customer_ids
    - lookup_list_manager_ids
    - lookup_list_operator_ids
    - lookup_list_sec_nodes

---
+++ msa_sdk/lookup.py
@@ -0,0 +1,96 @@
+"""Module lookup."""
+
+from msa_sdk.msa_api import MSA_API
+
+
+class Lookup(MSA_API):
+    """Class Lookup."""
+
+    def __init__(self):
+        """Initialize."""
+        MSA_API.__init__(self)
+        self.api_path_v1 = "/lookup/v1"
+        self.api_path = "/lookup"
+
+    def look_list_device_ids(self):
+        """Look list devices ids.
+
+        Returns
+        -------
+        None
+
+        """
+        self.action = 'Get device ids'
+        self.path = '{}/devices'.format(self.api_path)
+        self.call_get()
+
+    def look_list_customer_ids(self):
+        """Look list customer ids.
+
+        Returns
+        -------
+        None
+
+        """
+        self.action = 'Get customer ids'
+        self.path = '{}/customers'.format(self.api_path)
+        self.call_get()
+
+    def look_list_manager_ids(self):
+        """Look list manager ids.
+
+        Returns
+        -------
+        None
+
+        """
+        self.action = 'Get manager ids'
+        self.path = '{}/managers'.format(self.api_path)
+        self.call_get()
+
+    def look_list_operators_id(self, manager_id):
+        """Look list operators id.
+
+        Parameters
+        ----------
+        manager_id: Integer
+            Manager id
+
+        Returns
+        -------
+        None
+
+        """
+        self.action = 'Get operators id'
+        self.path = '{}/operators/id/{}'.format(self.api_path, manager_id)
+        self.call_get()
+
+    def look_list_sec_nodes(self):
+        """Look list sec nodes.
+
+        Returns
+        -------
+        None
+
+        """
+        self.action = 'Get sec nodes'
+        self.path = '{}/sec_nodes'.format(self.api_path)
+        self.call_get()
+
+    def look_list_device_by_customer_ref(self, custom_ref):
+        """Look list device by customer reference.
+
+        Parameters
+        ----------
+        manager_id: Integer
+            Manager id
+
+        Returns
+        -------
+        None
+
+        """
+        self.action = 'Get list device by customer reference'
+        self.path = '{}/customer/devices/reference/{}'.format(self.api_path,
+                                                              custom_ref)
+        self.call_get()
