823d2bb356309a1f279d98ab16457acdc68a0f3a
rory <rka@ubiqube.com>
2019-09-27 17:21:13 +0100

    Changing spaces to tabs and sorting the imports to keep Eduardo happy

--- msa_sdk/order.py
+++ msa_sdk/order.py
@@ -4,176 +4,176 @@ from msa_sdk.device import Device
 
 
 class Order(Device):
-	"""Class Order."""
+    """Class Order."""
 
-	def __init__(self, device_id):
-		"""Initialize."""
-		Device.__init__(self, device_id=device_id)
-		self.api_path = '/ordercommand'
-		self.read()
+    def __init__(self, device_id):
+        """Initialize."""
+        Device.__init__(self, device_id=device_id)
+        self.api_path = '/ordercommand'
+        self.read()
 
-	def command_execute(self, command, params, timeout=60):
-		"""
+    def command_execute(self, command, params, timeout=60):
+        """
 
-		Command execute.
+        Command execute.
 
-		Parameteres
-		-----------
-		command: String
-				Order command
+        Parameteres
+        -----------
+        command: String
+                Order command
 
-		params: dict
-			  Parameters
-
-
-		Returns
-		-------
-		None
-
-		"""
-		self.action = 'Command execute'
-		self.path = '{}/execute/{}/{}'.format(self.api_path, self.device_id,
-											  command)
-
-		self.call_post(params, timeout)
-
-	def command_generate_configuration(self, command, params):
-		"""
-
-		Command generate configuration.
-
-		Parameteres
-		-----------
-		command: String
-				Order command
-
-		params: dict
-			  Parameters
+        params: dict
+              Parameters
 
 
-		Returns
-		-------
-		None
+        Returns
+        -------
+        None
 
-		"""
-		self.action = 'Command generate configuration'
-		self.path = '{}/get/configuration/{}/{}'.format(self.api_path,
-														self.device_id,
-														command)
+        """
+        self.action = 'Command execute'
+        self.path = '{}/execute/{}/{}'.format(self.api_path, self.device_id,
+                                              command)
 
-		self.call_post(params)
+        self.call_post(params, timeout)
 
-	def command_synchronize(self, timeout):
-		"""
+    def command_generate_configuration(self, command, params):
+        """
 
-		Command synchronize.
+        Command generate configuration.
 
-		Parameteres
-		-----------
-		timeout: Integer
-			  Connection timeout
+        Parameteres
+        -----------
+        command: String
+                Order command
+
+        params: dict
+              Parameters
 
 
-		Returns
-		-------
-		None
+        Returns
+        -------
+        None
 
-		"""
-		self.action = 'Command synchronize'
-		self.path = '{}/synchronize/{}'.format(self.api_path,
-											   self.device_id)
+        """
+        self.action = 'Command generate configuration'
+        self.path = '{}/get/configuration/{}/{}'.format(self.api_path,
+                                                        self.device_id,
+                                                        command)
 
-		self.call_post(timeout=timeout)
+        self.call_post(params)
 
-	def command_call(self, command, mode, params):
-		"""
+    def command_synchronize(self, timeout):
+        """
 
-		Command call.
+        Command synchronize.
 
-		Parameters:
-		-----------
-		command: String
-				CRUID method in microservice to call
-		mode: Integer
-				0 - No application
-				1 - Apply to base
-				2 - Apply to device
-		Returns:
-		--------
-		None
+        Parameteres
+        -----------
+        timeout: Integer
+              Connection timeout
 
-		"""
-		self.action = 'Call command'
-		self.path = '{}/call/{}/{}/{}'.format(self.api_path,
-											self.device_id,
-											command,
-											mode)
-		self.call_post(params)
 
-	def command_objects_all(self):
-		"""
+        Returns
+        -------
+        None
 
-		Get all microservices attached to a device
+        """
+        self.action = 'Command synchronize'
+        self.path = '{}/synchronize/{}'.format(self.api_path,
+                                               self.device_id)
 
-		Parameters:
-		-----------
-		device_id: Integer
-				Device ID of the device
-		Returns:
-		--------
-		List:
-				List of names of microservices attached
+        self.call_post(timeout=timeout)
 
-		"""
-		self.action = 'Get Microservices'
-		self.path = '{}/objects/{}'.format(self.api_path, self.device_id)
-		self.call_get()
+    def command_call(self, command, mode, params):
+        """
 
-	def command_objects_instances(self, object_name):
-		"""
+        Command call.
 
-		Get microservices instance by microservice name
+        Parameters:
+        -----------
+        command: String
+                CRUID method in microservice to call
+        mode: Integer
+                0 - No application
+                1 - Apply to base
+                2 - Apply to device
+        Returns:
+        --------
+        None
 
-		Parameters:
-		-----------
-		device_id: Integer
-				Device ID of the device
-		object_name: String
-				Name of microservice
-		Returns:
-		--------
-		List:
-				List of object IDs per microservice
+        """
+        self.action = 'Call command'
+        self.path = '{}/call/{}/{}/{}'.format(self.api_path,
+                                            self.device_id,
+                                            command,
+                                            mode)
+        self.call_post(params)
 
-		"""
-		self.action = 'Get Microservice Instances'
-		self.path = '{}/objects/{}/{}'.format(self.api_path, 
-											self.device_id,
-											object_name)
-		self.call_get()
+    def command_objects_all(self):
+        """
 
-	def command_objects_instances_by_id(self, object_name, object_id):
-		"""
+        Get all microservices attached to a device.
 
-		Get microservices instance by microservice object ID
+        Parameters:
+        -----------
+        device_id: Integer
+                Device ID of the device
+        Returns:
+        --------
+        List:
+                List of names of microservices attached
 
-		Parameters:
-		-----------
-		device_id: Integer
-				Device ID of the device
-		object_name: String
-				Name of microservice
-		object_id: String
-				Object ID of microservice instance
-		Returns:
-		--------
-		List:
-				Object of microservice parameters per object ID
+        """
+        self.action = 'Get Microservices'
+        self.path = '{}/objects/{}'.format(self.api_path, self.device_id)
+        self.call_get()
 
-		"""
-		self.action = 'Get Microservice Object Details'
-		self.path = '{}/objects/{}/{}/{}'.format(self.api_path, 
-											self.device_id,
-											object_name,
-											object_id)
-		self.call_get()
+    def command_objects_instances(self, object_name):
+        """
+
+        Get microservices instance by microservice name.
+
+        Parameters:
+        -----------
+        device_id: Integer
+                Device ID of the device
+        object_name: String
+                Name of microservice
+        Returns:
+        --------
+        List:
+                List of object IDs per microservice
+
+        """
+        self.action = 'Get Microservice Instances'
+        self.path = '{}/objects/{}/{}'.format(self.api_path, 
+                                            self.device_id,
+                                            object_name)
+        self.call_get()
+
+    def command_objects_instances_by_id(self, object_name, object_id):
+        """
+
+        Get microservices instance by microservice object ID.
+
+        Parameters:
+        -----------
+        device_id: Integer
+                Device ID of the device
+        object_name: String
+                Name of microservice
+        object_id: String
+                Object ID of microservice instance
+        Returns:
+        --------
+        List:
+                Object of microservice parameters per object ID
+
+        """
+        self.action = 'Get Microservice Object Details'
+        self.path = '{}/objects/{}/{}/{}'.format(self.api_path, 
+                                            self.device_id,
+                                            object_name,
+                                            object_id)
+        self.call_get()
