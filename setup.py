83bea9d28b4eeabc2301b9278644c8134fa9464e
Eduardo Ferreira <efe@ubiqube.com>
2019-10-01 15:58:50 +0100

    Add urllib3 subpackages

--- setup.py
+++ setup.py
@@ -14,17 +14,28 @@ setup(
     license="",
     keywords="MSA",
     url="https://ubiqube.com",
-    packages=['msa_sdk', 'requests', 'urllib3', 'chardet', 'idna', 'certifi'],
-    package_dir={'requests': 'required_pkgs/requests',
-                 'urllib3': 'required_pkgs/urllib3',
-                 'chardet': 'required_pkgs/chardet',
-                 'idna': 'required_pkgs/idna',
-                 'certifi': 'required_pkgs/certifi'},
-    data_files=[('/opt/ses/share/htdocs/msa_sdk', DOC_HTML_FILES)],
+
+    packages=['msa_sdk', 'requests', 'chardet', 'chardet.cli',
+              'urllib3.packages',
+              'urllib3.contrib',
+              'urllib3.contrib._securetransport',
+              'urllib3.packages',
+              'urllib3.packages.backports',
+              'urllib3.packages.rfc3986',
+              'urllib3.packages.ssl_match_hostname',
+              'urllib3.util',
+              'idna', 'certifi'],
+    package_dir={
+        'requests': 'required_pkgs/requests',
+        'urllib3': 'required_pkgs/urllib3',
+        'chardet': 'required_pkgs/chardet',
+        'idna': 'required_pkgs/idna',
+        'certifi': 'required_pkgs/certifi'},
+    data_files=[
+        ('/opt/ses/share/htdocs/msa_sdk',
+         DOC_HTML_FILES)],
     include_package_data=True,
-    classifiers=[
-        "Development Status :: 4 - Beta"
-    ],
+    classifiers=["Development Status :: 4 - Beta"],
 )
 
 # vim: ft=python
