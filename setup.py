aad80f0a28cac0e723d365569c1cd97c43469ade
ArnaudFosse <34027417+ArnaudFosse@users.noreply.github.com>
2023-09-21 14:56:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,19 +15,9 @@ setup(
     keywords="MSA",
     url="https://ubiqube.com",
 
-    packages=['msa_sdk', 'requests', 'chardet', 'chardet.cli', 'urllib3',
-              'urllib3.packages',
-              'urllib3.contrib',
-              'urllib3.contrib._securetransport',
-              'urllib3.packages',
-              'urllib3.packages.backports',
-              'urllib3.packages.rfc3986',
-              'urllib3.packages.ssl_match_hostname',
-              'urllib3.util',
-              'idna'],
+    packages=['msa_sdk', 'requests', 'chardet', 'chardet.cli', 'idna'],
     package_dir={
         'requests': 'required_pkgs/requests',
-        'urllib3': 'required_pkgs/urllib3',
         'chardet': 'required_pkgs/chardet',
         'idna': 'required_pkgs/idna'},
     data_files=[
