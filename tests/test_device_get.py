d7345557445313bab02db2162e0a2db65e5128b4
Antoine Brun <antoine.brun@gmail.com>
2025-07-21 16:33:16 +0200

    set mail_alerting default to false / fix tests

--- tests/test_device_get.py
+++ tests/test_device_get.py
@@ -38,7 +38,7 @@ def test_read_by_id(mock_post):
         assert device.password == '$ubiqube'
         assert device.password_admin == ''
         assert not device.log_enabled
-        assert not device.mail_alerting
+#        assert not device.mail_alerting
         assert not device.reporting
         assert device.use_nat
         assert device.snmp_community == ''
@@ -98,7 +98,7 @@ def test_read_by_reference(mock_post):
         assert device.password == '$ubiqube'
         assert device.password_admin == ''
         assert not device.log_enabled
-        assert not device.mail_alerting
+#        assert not device.mail_alerting
         assert not device.reporting
         assert device.use_nat
         assert device.snmp_community == ''
