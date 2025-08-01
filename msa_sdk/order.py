df9a7c1861e1666d8f1fde6b72079cdd86b6c3e9
Saliou DIOP <sdi@ubiqube.com>
2025-08-01 09:49:27 +0200

    the MS stack management methods (create a stack, apply a stack) added

--- msa_sdk/order.py
+++ msa_sdk/order.py
@@ -42,8 +42,49 @@ class Order(Device):
         None
 
         """
-        self.action = 'Command execute'
+        self.action = 'Commands stack execute'
         self.path = '{}/execute/{}/{}'.format(self.api_path, self.device_id,
+                                              command)
+
+        self._call_post(params, timeout)
+
+    def apply_command_stack(self, command: str, params: dict, timeout=300) -> None:
+        """
+
+        Apply all commands stack of a device.
+
+        Parameters
+        -----------
+        command: String
+                Apply command
+                Available values : CREATE, UPDATE, IMPORT, LIST, READ, DELETE
+
+        params: dict
+                Parameters in a dict format:
+
+                {
+                    "simple_firewall": {
+                        "1": {
+                                "object_id": "1",
+                                "src_ip": "3.4.5.7",
+                                "dst_port": "44"
+                        },
+                        "2": {
+                                "object_id": "2",
+                                "src_ip": "3.4.5.6",
+                                "dst_port": "42"
+                        }
+                }
+                timeout:  int timeout in sec (300 secondes by default)
+
+        Returns
+        -------
+        None
+
+        """
+        self.action = 'Command execute'
+        self.api_path = '/orderstack'
+        self.path = '{}/command/{}/{}'.format(self.api_path, self.device_id,
                                               command)
 
         self._call_post(params, timeout)
@@ -183,6 +224,30 @@ class Order(Device):
                                               command,
                                               mode)
         self._call_post(params, timeout)
+
+
+    def command_stack(self, command: str, params,
+                     timeout=300) -> None:
+        """
+
+        Stack Command.
+
+        Parameters
+        -----------
+        command: String
+                CRUID method in microservice to add to the stack.
+        Returns
+        --------
+        None
+
+        """
+        self.action = 'Call command'
+        self.api_path = '/orderstack'
+        self.path = '{}/command/{}/{}'.format(self.api_path,
+                                              self.device_id,
+                                              command)
+        self._call_post(params, timeout)
+
 
     def command_objects_all(self) -> None:
         """
