14f3f7fb70b55a28c37a244bb006bc7db185c333
Saliou DIOP <sdi@ubiqube.com>
2025-08-04 10:29:31 +0200

    OPSLAB-69: suppression comments

--- tests/test_order_command_execute.py
+++ tests/test_order_command_execute.py
@@ -24,26 +24,6 @@ def test_command_execute(_, order_fixture):
 
         mock_call_post.assert_called_once_with({"subnet": "mySubnet"}, 50)
 
-
-# @patch('msa_sdk.device.Device.read')
-# def test_apply_command_stack(_, order_fixture):
-#     """
-#     Test Command apply command stack
-#     """
-
-#     local_path_command_stack = '/orderstack/command/21594/UPDATE'
-#     local_path_apply = '/orderstack/execute/21594'
-
-#     with patch('msa_sdk.msa_api.MSA_API._call_post') as mock_call_post:
-#         order = order_fixture
-#         order.command_stack('UPDATE', {"1": {"subnet": "mySubnet1"}, "2": {"subnet": "mySubnet2"}}, 50)
-#         assert order.path == local_path_command_stack
-#         mock_call_post.assert_called_with({"1": {"subnet": "mySubnet1"}, "2": {"subnet": "mySubnet2"}}, 50)
-#         order.apply_command_stack()
-#         assert order.path == local_path_apply
-
-        
-
 def test_command_execute_fail(order_fixture):
     """
     Test Command execute fail parameters type
