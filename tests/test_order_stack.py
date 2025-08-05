1c22935e74bbc6808455e1e2dae25c06acac2678
Saliou DIOP <sdi@ubiqube.com>
2025-08-05 13:01:08 +0200

    opslab 69: test_apply_command_stacked updated to match apply method params

--- tests/test_order_stack.py
+++ tests/test_order_stack.py
@@ -11,7 +11,7 @@ from util import orderstack_fixture  # pylint: disable=unused-import
 @patch('msa_sdk.device.Device.read')
 def test_add_command_in_stack(_, orderstack_fixture):
     """
-    Test Command apply command stack
+    Test Add command in the stack
     """
 
     local_path_command_stack = '/orderstack/command/21594/UPDATE'
@@ -25,4 +25,18 @@ def test_add_command_in_stack(_, orderstack_fixture):
         mock_call_put.assert_called_once_with({"1": {"subnet": "mySubnet1"}, "2": {"subnet": "mySubnet2"}}, 50)
         
 
+@patch('msa_sdk.device.Device.read')
+def test_apply_command_stacked(_, orderstack_fixture):
+    """
+    Test Apply command in the stack
+    """
 
+    local_path_apply = '/orderstack/execute/21594'
+
+    with patch('msa_sdk.msa_api.MSA_API._call_post') as mock_call_post:
+        orderstack = orderstack_fixture
+        orderstack.apply_command_stacked(timeout=50)
+
+        assert orderstack.path == local_path_apply
+
+        mock_call_post.assert_called_once_with(timeout=50)
