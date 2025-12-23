4ab04baa829c6a3b3d05dfc699aeaf6aa8864ac6
Olivier Vignaud <ovi@ubiqube.com>
2025-12-23 09:59:41 +0100

    🧪 MSA-15087: Fix unit test.

--- tests/test_profie.py
+++ tests/test_profie.py
@@ -11,10 +11,10 @@ def test_exist(profile_fixture):
     """
     Test exist profile by reference
     """
-    with patch('msa_sdk.msa_api.MSA_API._call_post') as mock_call_post:
+    with patch('msa_sdk.msa_api.MSA_API._call_get') as mock_call_get:
         profile = profile_fixture
         reference = "test1"
         profile.exist(reference)
 
-        assert profile.path == "/profile/v1/exist/test1"
-        mock_call_post.assert_called_once()
+        assert profile.path == "/profile/ref?extRef={}".format(reference)
+        mock_call_get.assert_called_once()
