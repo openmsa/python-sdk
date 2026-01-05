61967f1d03d782f6ccac1f08a0ca4a854340f2f1
pierrerondel <pro@ubiqube.com>
2026-01-05 11:26:28 +0100

    Update pattern_normalizer.py

--- transformers/pattern_normalizer.py
+++ transformers/pattern_normalizer.py
@@ -5,6 +5,7 @@ has a ``pattern`` field. Currently, this transformer acts as a pass-through.
 """
 
 from typing import Any, Dict
+
 from .base_transformer import BaseTransformer
 
 class PatternNormalizer(BaseTransformer):    
