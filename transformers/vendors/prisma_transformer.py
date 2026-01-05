e19b11b2fc52fe48aed277c8b263d16b1c9f31a4
pierrerondel <pro@ubiqube.com>
2026-01-05 13:51:11 +0100

    Update prisma_transformer.py

--- transformers/vendors/prisma_transformer.py
+++ transformers/vendors/prisma_transformer.py
@@ -12,7 +12,6 @@ from transformers.metadata_enricher import MetadataEnricher
 from transformers.pattern_normalizer import PatternNormalizer
 from transformers.type_mapper import TypeMapper
 
-
 # ---------------- PRISMA MAPPINGS ----------------
 
 PRISMA_ACTION_MAP = {
