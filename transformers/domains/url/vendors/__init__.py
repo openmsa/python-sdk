a519b3cadd4df9b7ad1a786e8eb8e6f7171c25f3
pierrerondel <pro@ubiqube.com>
2026-04-21 15:16:44 +0200

    Update __init__.py

--- transformers/domains/url/vendors/__init__.py
+++ transformers/domains/url/vendors/__init__.py
@@ -1,3 +1,9 @@
+"""
+Transformers package initialization.
+
+This package provides core functionality and public APIs for the transformers library.
+"""
+
 from . import fortinet
 
 # Optional: List of vendors for easy iteration in orchestration logic
