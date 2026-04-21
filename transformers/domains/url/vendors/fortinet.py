27706601ea6be32c9949d559c0b5068f90ba1ef9
pierrerondel <pro@ubiqube.com>
2026-04-21 16:06:03 +0200

    Update fortinet.py

--- transformers/domains/url/vendors/fortinet.py
+++ transformers/domains/url/vendors/fortinet.py
@@ -20,9 +20,9 @@ from transformers.framework.udm_transformers.category_mapper import \
     CategoryMapper
 from transformers.framework.udm_transformers.metadata_enricher import \
     MetadataEnricher
-from transformers.framework.udm_transformers.pattern_normalizer import \
-    PatternNormalizer
+from transformers.framework.udm_transformers.pattern_normalizer import PatternNormalizer
 from transformers.framework.udm_transformers.type_mapper import TypeMapper
+
 
 FORTINET_ACTION_MAP = {
     "allow": "allow",
