890f0d11b321245adb033d31866185ca367f3773
pierrerondel <pro@ubiqube.com>
2026-04-27 11:08:16 +0200

    Reorganize import statements in fortinet.py

--- transformers/domains/url/vendors/fortinet.py
+++ transformers/domains/url/vendors/fortinet.py
@@ -6,21 +6,27 @@ converting between Fortinet-specific configurations and the Pydantic
 Unified Data Model (UDM).
 """
 
-import jmespath
 from datetime import datetime
-from typing import Any, Dict, List, Optional
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
 
-# Framework imports - Absolute paths
-from transformers.framework.udm_transformers.action_mapper import ActionMapper
-from transformers.framework.udm_transformers.category_mapper import CategoryMapper
-from transformers.framework.udm_transformers.metadata_enricher import MetadataEnricher
-from transformers.framework.udm_transformers.pattern_normalizer import PatternNormalizer
-from transformers.framework.udm_transformers.type_mapper import TypeMapper
+import jmespath
 
 # Domain Model imports
 from transformers.domains.url.models import URL_UDM
 from transformers.domains.url.models import Category
 from transformers.domains.url.models import Metadata
+# Framework imports - Absolute paths
+from transformers.framework.udm_transformers.action_mapper import ActionMapper
+from transformers.framework.udm_transformers.category_mapper import \
+    CategoryMapper
+from transformers.framework.udm_transformers.metadata_enricher import \
+    MetadataEnricher
+from transformers.framework.udm_transformers.pattern_normalizer import \
+    PatternNormalizer
+from transformers.framework.udm_transformers.type_mapper import TypeMapper
 
 # ---------------- FORTINET MAPPINGS ----------------
 
